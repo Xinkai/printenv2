92425265d93cb7e7f4447358c2de8b6385894df9
Xinkai Chen <xinkai.chen@qq.com>
2022-08-07 22:57:25 +0800

    Always use the same clang target as rust target.

--- build.rs
+++ build.rs
@@ -2,6 +2,19 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
+fn bindgen_generate(header: &str) -> Result<bindgen::Bindings, bindgen::BindgenError> {
+    let target = env::var("TARGET");
+
+    let mut result = bindgen::Builder::default()
+        .header(header)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks));
+
+    if let Ok(target) = target {
+        result = result.clang_arg(format!("--target={}", target));
+    }
+    result.generate()
+}
+
 fn main() {
     let unix = env::var("CARGO_CFG_UNIX");
     let os = env::var("CARGO_CFG_TARGET_OS");
@@ -15,11 +28,7 @@ fn main() {
         (Ok(_), Ok("macos")) => {
             println!("cargo:rerun-if-changed=src/apple-sysctl-wrapper.h");
 
-            if let Ok(sysctl_bindings) = bindgen::Builder::default()
-                .header("src/apple-sysctl-wrapper.h")
-                .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-                .generate()
-            {
+            if let Ok(sysctl_bindings) = bindgen_generate("src/apple-sysctl-wrapper.h") {
                 let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
                 sysctl_bindings
                     .write_to_file(out_path.join("apple-sysctl-bindings.rs"))
@@ -33,11 +42,7 @@ fn main() {
         (Ok(_), _) => {
             println!("cargo:rerun-if-changed=src/kvm-wrapper.h");
 
-            if let Ok(kvm_bindings) = bindgen::Builder::default()
-                .header("src/kvm-wrapper.h")
-                .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-                .generate()
-            {
+            if let Ok(kvm_bindings) = bindgen_generate("src/kvm-wrapper.h") {
                 println!("cargo:rustc-link-lib=kvm");
                 let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
                 kvm_bindings
